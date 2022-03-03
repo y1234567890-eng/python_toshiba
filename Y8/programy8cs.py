86eeee10682a04035a5cb4eec710b62b67df53ad
Yousuf <myousufjamil@hotmail.com>
2022-03-03 12:19:51 +0300

    Made a Program

--- Y8/programy8cs.py
+++ Y8/programy8cs.py
@@ -14,41 +14,84 @@ def processUserLogin():
             print("Please enter correct username and password.")
 
 
+processUserLogin()
+
+print()
+
 #enter 6 lengths
+print("Enter six lengths below:")
 
-one = int()
-two = int()
+print()
 
-def enterSixLengths():
-    one = int(input("Enter length 1: "))
-    two = int(input("Enter length 2: "))
-    print("Other lengths skipped")
+one = str(input("Enter length 1: "))
+two = str(input("Enter length 2: "))
+print("Other lengths skipped")
+
+print()
+
 
 #sort lengths
 
-length = str()
+if(one>two):
+    print("Length 1 is bigger")
+    length = str("Length 1 is bigger")
+elif(one<two):
+    print("Length 2 is bigger")
+    length = str("Length 2 is bigger")
+elif(one==two):
+    print("Both lengths are equal")
+    length=str("Both Lengths are equal")
+else:
+    print("Unknown error in sorting lengths")
 
-def sortLengths():
-    if(one>two):
-        print("Length 1 is bigger")
-        length = str("L1 big")
-    elif(one<two):
-        print("Length 2 is bigger")
-        length = str("L2 big")
-    else:
-        print("Unknown error in sorting lengths")
+print()
+
 
 #show quiz
+
+print("Quiz time!")
+print()
+
+q1 = str(input("How can cold water affect your swimming capability? \n a. It does not \n b. It is harder to swim as you get cramp \n c. It is easier to swim as you cool down \nType your correct answer (a, b, c, d): "))
+
+if(q1 == "b"):
+        print("Correct Answer")
+        answer = str("b, correct")
+else:
+    print("Incorrect answer")
+    answer = str("incorrect option, incorrect")
+print("Other questions skipped")
+
+
 
 
 
 #show results
-
-
+print("Your Result:")
+print()
+def results():
+    print("-----------------------------------------------------")
+    print("|              Riverside Swimming Club               |")
+    print("| Length 1: " + one + "                                        |")
+    print("| Length 2: " + two + "                                        |")
+    print("| Bigger length: " + length + "                  |")
+    print("| Quiz: " + answer + "                                   |")
+    print("-----------------------------------------------------")
 
 #call functions
-processUserLogin()
-enterSixLengths()
-enterSixLengths()
 
-input("Press enter to exit")
+results()
+
+print()
+print("Thank you for using our app!")
+print()
+log_out = "no"
+while(log_out == "no"):
+    input("Press enter log out, clear data and exit")
+    exit = input("Are you sure you want to exit (Y/N): ")
+    if(exit=="N"):
+        log_out = "no"
+    elif(exit=="Y"):
+        log_out = "yes"
+    else:
+        print("Type Y/N only")
